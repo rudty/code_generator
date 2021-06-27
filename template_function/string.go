3624380878d2604a66772b87c81126e910c3eb55
석경우 <rudtyz@gmail.com>
2021-06-27 11:57:42 +0900

    string 추가

--- template_function/string.go
+++ template_function/string.go
@@ -41,20 +41,26 @@ func Join(sep string, v interface{}) string {
 		reflect.Slice:
 		l := r.Len()
 		for i := 0; i < l; i++ {
+			if i > 0 {
+				b.WriteString(sep)
+			}
 			e := r.Index(i).Interface()
 			b.WriteString(fmt.Sprint(e))
-			b.WriteString(sep)
 		}
 	case reflect.Map:
 		iter := r.MapRange()
-		for iter.Next() {
+		for i := 0; iter.Next(); i++ {
 			mk := iter.Key().Interface()
 			mv := iter.Value().Interface()
+			if i > 0 {
+				b.WriteString(sep)
+			}
 			b.WriteString(fmt.Sprint(mk))
 			b.WriteString(sep)
 			b.WriteString(fmt.Sprint(mv))
-			b.WriteString(sep)
 		}
+	case reflect.String:
+		return v.(string)
 	}
 	return b.String()
 }
@@ -105,4 +111,92 @@ func RemoveFirst(v interface{}) string {
 		return b.String()
 	}
 	return ""
+}
+
+// Concat 인자로 들어온 값들을 모두 더합니다
+func Concat(v ...interface{}) string {
+	return Join("", v)
+}
+
+func toUpperByte(c byte) byte {
+	if 'a' <= c && c <= 'z' {
+		c -= 'a' - 'A'
+	}
+	return c
+}
+
+func toLowerByte(c byte) byte {
+	if 'A' <= c && c <= 'Z' {
+		c += 'a' - 'A'
+	}
+	return c
+}
+
+func removeUnderLineInternal(b *strings.Builder, name string) string {
+	toUpper := false
+
+	for i := 1; i < len(name); i++ {
+		switch name[i] {
+		case 45: // '-'
+		case 95: // '_'
+			toUpper = true
+		default:
+			elem := name[i]
+			if toUpper {
+				elem = toUpperByte(elem)
+				toUpper = false
+			}
+			b.WriteByte(elem)
+		}
+	}
+
+	return b.String()
+}
+
+func ToPascal(v interface{}) string {
+	name := Join(",", v)
+	if len(name) == 0 {
+		return ""
+	}
+
+	b := strings.Builder{}
+	firstChar := toUpperByte(name[0])
+	b.WriteByte(firstChar)
+
+	return removeUnderLineInternal(&b, name)
+}
+
+func ToCamel(v interface{}) string {
+	name := Join(",", v)
+	if len(name) == 0 {
+		return ""
+	}
+
+	b := strings.Builder{}
+	firstChar := toLowerByte(name[0])
+	b.WriteByte(firstChar)
+
+	return removeUnderLineInternal(&b, name)
+}
+
+func ToSnake(v interface{}) string {
+	name := Join(",", v)
+	if len(name) == 0 {
+		return ""
+	}
+
+	builder := strings.Builder{}
+	firstChar := toLowerByte(name[0])
+	builder.WriteByte(firstChar)
+
+	for i := 1; i < len(name); i++ {
+		if 'A' <= name[i] && name[i] <= 'Z' {
+			builder.WriteByte(95)
+			builder.WriteByte(toLowerByte(name[i]))
+		} else {
+			builder.WriteByte(name[i])
+		}
+	}
+
+	return builder.String()
 }
--- template_function/string_test.go
+++ template_function/string_test.go
@@ -48,8 +48,8 @@ func TestRepeatMap(t *testing.T) {
 
 func TestJoinSlice(t *testing.T) {
 	a := Join(".", []int{1, 2, 3, 4, 5})
-	if a != "1.2.3.4.5." {
-		t.Error("1.2.3.4.5./" + a)
+	if a != "1.2.3.4.5" {
+		t.Error("1.2.3.4.5/" + a)
 	}
 }
 
@@ -57,8 +57,8 @@ func TestJoinMap(t *testing.T) {
 	a := Join(".", map[string]interface{}{
 		"hello": "world",
 	})
-	if a != "hello.world." {
-		t.Error("hello.world./" + a)
+	if a != "hello.world" {
+		t.Error("hello.world/" + a)
 	}
 }
 
@@ -87,5 +87,89 @@ func TestRemoveFirstSlice(t *testing.T) {
 	a := RemoveFirst([]int{100, 200, 300, 400, 500})
 	if a != "200300400500" {
 		t.Error("200300400500/" + a)
+	}
+}
+
+func Test_to_snake_1(t *testing.T) {
+	c := ToSnake("HelloWorld")
+	if c != "hello_world" {
+		t.Error("hello_world" + c)
+	}
+}
+
+func Test_to_snake_2(t *testing.T) {
+	c := ToSnake("GOLANG")
+	if c != "g_o_l_a_n_g" {
+		t.Error("g_o_l_a_n_g" + c)
+	}
+}
+
+func Test_to_snake_3(t *testing.T) {
+	c := ToSnake("HE_llo")
+	if c != "h_e_llo" {
+		t.Error("h_e_llo/" + c)
+	}
+}
+
+func Test_to_snake_4(t *testing.T) {
+	c := ToSnake("h_e_l_l_o_w_o_r_l_d")
+	if c != "h_e_l_l_o_w_o_r_l_d" {
+		t.Error("h_e_l_l_o_w_o_r_l_d/" + c)
+	}
+}
+
+func TestToPascal1(t *testing.T) {
+	c := ToPascal("HelloWorld")
+	if c != "HelloWorld" {
+		t.Error("HelloWorld" + c)
+	}
+}
+
+func TestToPascal2(t *testing.T) {
+	c := ToPascal("g_o_l_a_n_g")
+	if c != "GOLANG" {
+		t.Error("GOLANG" + c)
+	}
+}
+
+func TestToPascal3(t *testing.T) {
+	c := ToPascal("HE_llo")
+	if c != "HELlo" {
+		t.Error("HELlo/" + c)
+	}
+}
+
+func TestToPascal4(t *testing.T) {
+	c := ToPascal("h_e_l_l_o_w_o_r_l_d")
+	if c != "HELLOWORLD" {
+		t.Error("HELLOWORLD/" + c)
+	}
+}
+
+func TestToCamel1(t *testing.T) {
+	c := ToCamel("HelloWorld")
+	if c != "helloWorld" {
+		t.Error("helloWorld" + c)
+	}
+}
+
+func TestToCamel2(t *testing.T) {
+	c := ToCamel("g_o_l_a_n_g")
+	if c != "gOLANG" {
+		t.Error("gOLANG" + c)
+	}
+}
+
+func TestToCamel3(t *testing.T) {
+	c := ToCamel("HE_llo")
+	if c != "hELlo" {
+		t.Error("hELlo/" + c)
+	}
+}
+
+func TestToCamel4(t *testing.T) {
+	c := ToCamel("h_e_l_l_o_w_o_r_l_d")
+	if c != "hELLOWORLD" {
+		t.Error("hELLOWORLD/" + c)
 	}
 }
