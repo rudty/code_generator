c3e77fd446127ceab01d9fea7568237e65b7c1c6
석경우 <rudtyz@gmail.com>
2021-07-19 17:44:26 +0900

    자바 타입 추가

--- F/type.js
+++ F/type.js
@@ -6,13 +6,13 @@ const toJavaType = (t) => {
         if (t.includes("date")) {
             return "LocalDateTime";
         }
-        if (t.includes("byte") || t.includes("int8")) {
+        if (t.includes("byte") || t.includes("int8") || t.includes("tinyint")) {
             return "byte";
         }
-        if (t.includes("smallint") || t.includes("int16")) {
+        if (t.includes("smallint") || t.includes("int16") || t.includes("smallint")) {
             return "short";
         }
-        if (t.includes("int64") || t.includes("long")) {
+        if (t.includes("int64") || t.includes("long") || t.includes("bigint")) {
             return "long";
         }
         if (t.includes("int32") || t.includes("int")) {
@@ -24,7 +24,7 @@ const toJavaType = (t) => {
         if (t.includes("float32") || t.includes("float")) {
             return "float";
         }
-        if (t.includes("string") || t.includes("varchar")) {
+        if (t.includes("string") || t.includes("varchar") || t.includes("text")) {
             return "String";
         }
     }
